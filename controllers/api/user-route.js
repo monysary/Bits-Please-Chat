113b7e74ecacb2fed7e2bddaca82fd74bb7e0fa8
sdanimc <116037382+sdanimc@users.noreply.github.com>
2023-01-06 13:44:11 -0800

    when sign up code now checks for existing users with same email

--- controllers/api/user-route.js
+++ controllers/api/user-route.js
@@ -3,20 +3,28 @@ const { User } = require('../../models');
 
 // User Signup Route
 router.post('/signup', async (req, res) => {
+  const userData ={
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+  }
   try {
-    const userData = await User.create(
-      {
-        username: req.body.username,
+    const userEmail = await User.findOne({
+     where: {
         email: req.body.email,
-        password: req.body.password,
-      }
-    );
-    req.session.save(() => {
-      req.session.user_id = userData.id;
-      req.session.username = userData.username;
-      req.session.loggedIn = true;
-      res.status(200).json(userData);
-    });
+      },}
+    ); 
+    if(userEmail){
+      res.status(400).json({ message:  'Account already exists for this email' });
+        return;
+    } else{
+      User.create(userData);
+      req.session.save(() => {
+        req.session.username = userData.username;
+        req.session.loggedIn = true;
+        res.status(200).json(userData);
+      })
+    }
   } catch (err) {
     console.log(err);
     res.status(400).json(err);
