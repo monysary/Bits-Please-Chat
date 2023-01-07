1edddb5240f89fbcb8615e8935a53738a9e9f62f
monysary <sary.mony@gmail.com>
2023-01-06 21:34:37 -0800

    updated code

--- config/connections.js
+++ config/connections.js
@@ -2,8 +2,8 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 // create connection to our db
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
+const sequelize = process.env.CLEARDB_DATABASE_URL
+  ? new Sequelize(process.env.CLEARDB_DATABASE_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
       host: '127.0.0.1',
       dialect: 'mysql',
